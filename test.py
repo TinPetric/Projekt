1d1806316c76272631583edc19f0951793596d7d
TinPetric <72347919+TinPetric@users.noreply.github.com>
2021-06-08 17:35:28 +0200

    Add files via upload
    radiobutton upgrade

--- test.py
+++ test.py
@@ -1,39 +1,17 @@
-from math import floor
+import tkinter as tk
+from PIL import ImageTk, Image
 
-import numpy as np
-import cv2
-import matplotlib.pyplot as plt
+path = "ucenje\\"
 
-def readButton(img):
-    #img = cv2.imread("segmenti\\" + str(i) +"Bodovi.jpg")
+i = 358
+for i in range(2500,2633):
+    print(str(i + 1))
+    ime = path + str(i + 1) + ".jpg"
+    root = tk.Tk()
 
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img = ImageTk.PhotoImage(Image.open(ime))
+    label = tk.Label(root, image = img).pack()
 
-    blurred = cv2.medianBlur(gray, 25) #cv2.bilateralFilter(gray,10,50,50)
+    root.after(1000, lambda: root.destroy())
+    root.mainloop()
 
-    minDist = 100
-    param1 = 10 #500
-    param2 = 18 #200 #smaller value-> more false circles
-    minRadius = 5
-    maxRadius = 50 #10
-
-    # docstring of HoughCircles: HoughCircles(image, method, dp, minDist[, circles[, param1[, param2[, minRadius[, maxRadius]]]]]) -> circles
-    circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, 1, minDist, param1=param1, param2=param2, minRadius=minRadius, maxRadius=maxRadius)
-    size = img.shape
-    xvalue = size[1]/16
-    print("xvalue: ",xvalue)
-    result=None
-    print("circles",circles)
-    if circles is not None:
-        circles = np.uint16(np.around(circles))
-        for i in circles[0,:]:
-            cv2.circle(img, (i[0], i[1]), i[2], (0, 255, 0), 2)
-            result=floor(i[0]/ xvalue)
-    print("result",result)
-
-    # Show result for testing:
-
-    plt.imshow(img, cmap=plt.cm.binary)
-    plt.show()
-    cv2.destroyAllWindows()
-    return result
